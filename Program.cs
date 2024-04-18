42694d14d2e26dfe1fbc9d560d5bcbe25f80824a
Alok Sharma <56442060+alok744@users.noreply.github.com>
2024-04-19 02:12:41 +0530

    output
    *
    **
    ***
    ****
    *****
    ****
    ***
    **
    *

--- Program.cs
+++ Program.cs
@@ -1,18 +1,27 @@
-﻿using System.Globalization;
-namespace pattern_project
+﻿namespace pattern_project
 {
     class program
     {
-        static void Main(String[] args) 
-        {          
+        static void Main(String[] args)
+        {
+           
             for (int i = 1; i <= 5; i++)
             {
-                for (int j = 1; j <= 5; j++)
+                for (int j = 1; j <= i; j++)
                 {
                     Console.Write("*");
                 }
                 Console.WriteLine();
             }
+            for (int i = 1; i <= 4; i++)
+            {
+                for (int j = i; j <= 4; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
             Console.ReadLine();
         }
 
